dad3960a1618e6ff75f18047761342dc5ae02fed
MaximVladimirov <maxsposad@mail.ru>
2019-03-13 21:31:18 +0300

    Add SummArray.c

---
+++ SummArray.c
@@ -0,0 +1,41 @@
+/*
+
+***********Summ array recursive*************
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumarray(int *arr,int size);
+
+int main(){
+
+	int array[] = {1,2,1,1,1,1,1,1,1,1};
+
+	int size = sizeof(array)/sizeof(int);
+
+	int summ = sumarray(array,size);
+
+	printf("%d\n", size);
+	printf("%d\n", summ);
+
+	return 0;
+
+}
+
+int sumarray(int *arr,int size){
+
+	int array[size - 1];
+
+	for(int i = 0; i < size - 1; i++)
+		array[i] = arr[i + 1];
+
+	if (size == 1) {
+		return arr[0];
+	}
+
+	size = size - 1;
+
+	return arr[0] + sumarray(array,size);
+
+}
